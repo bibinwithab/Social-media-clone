3f430d98ee96c03a44178d76e889c5b7fd74d622
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-10-01 11:39:11 +0530

    dotenv and init setup done

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server up in http://localhost:${PORT}`);
+})
